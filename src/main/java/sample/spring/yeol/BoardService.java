d4e1c0cd821a50c08c772c93eafbd4e1d2ce1770
Ye0l <casiya03@gmail.com>
2021-12-14 18:19:42 +0900

    board service writepost 마무리하고 이어할것

--- src/main/java/sample/spring/yeol/BoardService.java
+++ src/main/java/sample/spring/yeol/BoardService.java
@@ -1,5 +1,9 @@
 package sample.spring.yeol;
 
+import java.util.Map;
+
 public interface BoardService {
+
+	String writePost(Map<String, Object> map);
 
 }
--- src/main/java/sample/spring/yeol/BoardServiceImpl.java
+++ src/main/java/sample/spring/yeol/BoardServiceImpl.java
@@ -1,5 +1,7 @@
 package sample.spring.yeol;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +9,14 @@ import org.springframework.stereotype.Service;
 public class BoardServiceImpl implements BoardService{
 	@Autowired
 	BoardDao boardDao;
+	
+	@Override
+	public String writePost(Map<String, Object> map) {
+		int affectRowCount = this.boardDao.insert(map);
+		if(affectRowCount == 1) {
+			return map.get("board_id").toString();
+		}
+		
+		return null;
+	}
 }
