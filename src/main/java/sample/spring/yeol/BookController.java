4f6c1c27ab62af8f82181459044744cf3432e914
Ye0l <casiya03@gmail.com>
2021-12-14 14:18:10 +0900

    Spring 연습

---
+++ src/main/java/sample/spring/yeol/BookController.java
@@ -0,0 +1,37 @@
+package sample.spring.yeol;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BookController {
+	@Autowired
+	BookService bookService;
+	// 대체 왜 BookServiceImpl 클래스가 아니라 BookService 인터페이스를 생성하는가.. 모르겠다.
+	// 나중에 다시 읽어보기 https://okky.kr/article/413840
+	
+	@RequestMapping(value="/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		return new ModelAndView("book/create");
+	}
+	
+	@RequestMapping(value="/create", method = RequestMethod.POST)
+	public ModelAndView createPost(@RequestParam Map<String, Object> map) {
+		ModelAndView mav = new ModelAndView();
+		
+		String bookId = this.bookService.create(map);
+		if(bookId == null) {
+			mav.setViewName("redirect:/create");
+		} else {
+			mav.setViewName("redirect:/detail?bookId=" + bookId);
+		}
+		
+		return mav;
+	}
+}
---
+++ src/main/java/sample/spring/yeol/BookService.java
@@ -0,0 +1,9 @@
+package sample.spring.yeol;
+
+import java.util.Map;
+
+public interface BookService {
+
+	String create(Map<String, Object> map);
+
+}
